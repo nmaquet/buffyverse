253872a9043e96b2fc165d4a1e71cc823abf4f63
maddiebrighouse <madeleinebrighouse@gmail.com>
2023-05-03 16:55:43 +1200

    Tailwind set up

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>Buffyverse</h1>
+      <h1 className="text-3xl font-bold underline">Buffyverse</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
