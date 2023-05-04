7e41788ab0b568bf461ec89592e6777e6337ef6e
maddiebrighouse <madeleinebrighouse@gmail.com>
2023-05-04 22:02:14 +1200

    main page min

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { useState } from "react";
-import "./App.css";
+import Card from "./components/Card";
+import people from "./people.json";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Buffyverse</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="py-24 sm:py-32 bg-fixed bg-cover bg-center bg-[url('src/assets/944296.jpg')]">
+      <div className="px-6 mx-auto text-center max-w-7xl lg:px-8">
+        <ul
+          role="list"
+          className="grid max-w-2xl grid-cols-1 gap-6 mx-auto mt-20 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-8"
+        >
+          {people.people.map((person) => (
+            <Card {...person} />
+          ))}
+        </ul>
       </div>
-    </>
+    </div>
   );
 }
 
