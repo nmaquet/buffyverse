dfdd4a5875b6d109b1f36de63ba3fdf321ac45f7
maddiebrighouse <madeleinebrighouse@gmail.com>
2023-05-05 23:23:27 +1200

    header and backdrop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const Header: React.FC = () => {
   return (
-    <div className="relative">
+    <div className="pb-96">
       <img
-        className="absolute mr-72 right-80 opacity-80"
-        src="src/assets/moon.png"
+        className="absolute ml-32 w-96 left-96"
+        src="src/assets/header/moon.png"
         alt="Buffy the Vampire Slayer"
       />
       <img
-        className="w-1/3 mx-auto mix-blend-lighten invert"
-        src="src/assets/BF-0797b-1.png"
+        className="absolute w-1/3 mt-20 ml-64 opacity-80 mix-blend-light"
+        src="src/assets/header/title.png"
         alt="Buffy the Vampire Slayer"
       />
       <div className="absolute top-4 right-6">
