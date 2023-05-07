0418c32be69445acb7eabf3b1c6a7b5e0727e351
maddiebrighouse <madeleinebrighouse@gmail.com>
2023-05-07 23:36:35 +1200

    google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        almendra: ["Almendra", ...defaultTheme.fontFamily.sans],
-        grenze: ["Grenze", ...defaultTheme.fontFamily.sans],
-        lacquer: ["Lacquer", ...defaultTheme.fontFamily.sans],
-        longCang: ["Long Cang", ...defaultTheme.fontFamily.sans],
+        almendra: ["Almendra", "serif"],
+        grenze: ["Grenze", "cursive"],
+        lacquer: ["Lacquer", "cursive"],
+        longCang: ["Long Cang", "cursive"],
       },
     },
   },
