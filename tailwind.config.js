b1c91eddc7289e7b7e9ea48268c00356629a0305
maddiebrighouse <madeleinebrighouse@gmail.com>
2023-05-07 19:54:53 +1200

    Add Google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        almendra: ["Almendra", ...defaultTheme.fontFamily.sans],
+        grenze: ["Grenze", ...defaultTheme.fontFamily.sans],
+        lacquer: ["Lacquer", ...defaultTheme.fontFamily.sans],
+        longCang: ["Long Cang", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
