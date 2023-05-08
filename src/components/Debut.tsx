f1e5a86339a630439edd2e87b32789ce9570b2a3
maddiebrighouse <madeleinebrighouse@gmail.com>
2023-05-09 10:16:52 +1200

    WIP: Styling

--- src/components/Debut.tsx
+++ src/components/Debut.tsx
@@ -3,8 +3,9 @@ import { Debut as DebutType } from "../interfaces";
 const Debut = ({ season, episode }: DebutType) => {
   return (
     <div className="flex justify-center">
-      <p className="text-2xl text-black font-lacquer">
-        Debut: E0{episode} S0{season}
+      <p className="text-2xl text-black font-grenze">Debut: </p>
+      <p className="pl-2 text-2xl text-red-700 font-grenze">
+        EO{episode}SO{season}
       </p>
     </div>
   );
