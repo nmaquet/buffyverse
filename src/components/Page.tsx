b6e7888611af4e0cc8193939f389c344c1f521cc
maddiebrighouse <madeleinebrighouse@gmail.com>
2023-05-08 19:43:11 +1200

    stlying

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -3,17 +3,17 @@ const Page = ({ title = "About" }) => {
   return (
     <div className="bg-[url('src/assets/frames/texture.jpg')] w-2/3 h-2/3 rounded-3xl bg-cover p-3">
       <div className="p-3 m-auto border-2 rounded-3xl border-amber-400">
-        <div className="p-2 mb-2 text-4xl text-center border-b-2 font-almendra border-amber-400">
+        <div className="p-2 mb-2 text-4xl text-center border-b-2 font-lacquer border-amber-400">
           {title}
         </div>
-        <p>
+        <p className="text-2xl font-lacquer">
           Front-end project: <br />
           {frontendSpec}
         </p>
         <br />
-        <p>{demonstrate}</p>
+        <p className="text-2xl font-lacquer">{demonstrate}</p>
         <br />
-        <p> {skills}</p>
+        <p className="text-2xl font-lacquer"> {skills}</p>
         <br />
       </div>
     </div>
