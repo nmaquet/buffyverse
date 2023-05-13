17d7d6ca844fbb977f383a17a883a10b106d06f9
maddiebrighouse <madeleinebrighouse@gmail.com>
2023-05-14 02:09:21 +1200

    update styleing nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,14 +3,11 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <div className="flex">
-      <Link
-        to="/doc"
-        className="p-4 text-2xl text-white underline font-lacquer"
-      >
+      <Link to="/doc" className="p-4 text-3xl text-white font-lacquer">
         Doc
       </Link>
       <Link
-        className="p-4 text-2xl text-white underline cursor-pointer font-lacquer"
+        className="p-4 text-3xl text-white cursor-pointer font-lacquer"
         to="/about"
       >
         About
