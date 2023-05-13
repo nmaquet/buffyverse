4abc3568532c97e1e71077ba5321972a927cad9d
maddiebrighouse <madeleinebrighouse@gmail.com>
2023-05-14 02:09:34 +1200

    add badge

--- src/components/Species.tsx
+++ src/components/Species.tsx
@@ -1,4 +1,19 @@
-const Species = ({ species }: any) => {
+export const Badge = (icon: string) => {
+  return (
+    <div className="relative">
+      <div className="absolute w-12 h-12 rounded-full -top-6 left-28 bg-amber-400 ">
+        <img
+          className="absolute w-8 h-10 pt-2 pl-2 left-1 opacity-20"
+          src={`src/assets/icons/${icon}.png`}
+        />
+        <p className="pt-2 pl-1 text-2xl text-black font-grenze"></p>
+      </div>
+    </div>
+  );
+};
+
+// TODO fail back to default image
+export const Species = (species: string) => {
   return (
     <div className="flex justify-center pt-6">
       <img
@@ -10,5 +25,3 @@ const Species = ({ species }: any) => {
     </div>
   );
 };
-
-export default Species;
