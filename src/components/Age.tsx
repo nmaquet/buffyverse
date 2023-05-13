eaef7150b4c346dcd06ddb95ffd4d7a6cc78bc68
maddiebrighouse <madeleinebrighouse@gmail.com>
2023-05-14 02:08:56 +1200

    WIP Age function

--- src/components/Age.tsx
+++ src/components/Age.tsx
@@ -1,13 +1,28 @@
-const Age = ({ age }: number) => {
+//TODO: DEBUG FUNCTION
+function getAge(dateStr: Date) {
+  const endOfShow = new Date("2002-10-23");
+  const birthDate = new Date(dateStr);
+  let age = endOfShow.getFullYear() - birthDate.getFullYear();
+  const m = endOfShow.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && endOfShow.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
+}
+
+type Props = {
+  birthday: number | string;
+};
+const Age = ({ birthday }: Props) => {
+  const age = getAge(birthday);
   return (
     <div className="relative">
-      <div className="absolute w-full p-px border-2 bg-amber-400" />
       <div className="absolute w-12 h-12 rounded-full -top-6 left-28 bg-amber-400 ">
         <img
           className="absolute w-8 h-10 pt-2 pl-2 left-1 opacity-20"
           src={"src/assets/icons/age.png"}
         />
-        <p className="pt-2 pl-1 text-2xl text-black font-grenze">{age}</p>
+        <p className="pt-2 pl-1 text-2xl text-black font-grenze"></p>
       </div>
     </div>
   );
