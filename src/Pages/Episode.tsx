1bab23663d1009fb6f0d00e8f88f2380b39c8293
maddiebrighouse <madeleinebrighouse@gmail.com>
2023-05-14 21:48:48 +1200

    episode component

---
+++ src/Pages/Episode.tsx
@@ -0,0 +1,13 @@
+const Episode = () => {
+  return (
+    <div className="w-2/3 p-10 m-10 bg-cover border-4 bg-texture rounded-2xl border-amber-400 animate-fadeUp">
+      <div className="text-4xl font-almendra ">Episode</div>
+      The Sunnydale Graveyard is a location where many supernatural events take
+      place. It is a common location for vampires to rise from their graves, and
+      a frequent site of battles between the Scooby Gang and various
+      supernatural foes.
+    </div>
+  );
+};
+
+export default Episode;
